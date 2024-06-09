3ca935bd30b5c6d60a59e9898f736c82e0376a1c
BeomBeom <62642155+BeomBeom2@users.noreply.github.com>
2024-06-10 01:02:36 +0900

    login - Add Login check logic & Login, Signup Activity compose

---
+++ app/src/main/java/com/example/myapplication/DB/Person.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.DB
+
+import java.time.LocalDate
+
+data class Person(
+    var id:String,
+    var pw:String,
+    var name:String,
+    var age:Int,
+    var height:Double,
+    var weight:Double,
+    var kcal:Int,
+    var carbohydrate:Double,
+    var protein:Double,
+    var fat:Double,
+    var intake: Map<String, Nutrient>
+) {
+    constructor():this("", "", "", 0, 0.0, 0.0, 0, .0, .0, .0, mapOf())
+}
