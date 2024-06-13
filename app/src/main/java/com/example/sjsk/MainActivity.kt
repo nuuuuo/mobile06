f854c88de3aa35200c33e90d4ac81e9c03c5413c
Seung Hyeon <goldi0609@gmail.com>
2024-06-14 01:17:36 +0900

    nn

---
+++ app/src/main/java/com/example/sjsk/MainActivity.kt
@@ -0,0 +1,212 @@
+package com.example.sjsk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.FlowRowScopeInstance.weight
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.sjsk.firestore.ApiResponse
+import com.example.sjsk.firestore.RetrofitClient
+import com.example.sjsk.ui.theme.SJSKTheme
+import com.example.sjsk.viewModel.Repository
+import com.example.sjsk.viewModel.SearchViewModel
+import com.example.sjsk.viewModel.ViewModelFactory
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SJSKTheme {
+//                val mainViewModel: SjskViewModel = viewModel()
+//                MyAppScreen(mainViewModel)
+
+                val viewModel: SearchViewModel = viewModel(factory = ViewModelFactory(Repository(RetrofitClient.instance)))
+                MyAppScreen(viewModel)
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyAppScreen(viewModel: SearchViewModel) {
+    var searchText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                label = { Text("Search Food") },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(IntrinsicSize.Min),
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Button(
+                onClick = { viewModel.searchFood(searchText) },
+                modifier = Modifier.height(IntrinsicSize.Min)
+//                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF3F51B5))
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.baseline_search_24),
+                    contentDescription = "Search",
+                    tint = Color.White
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        val data by viewModel.data.collectAsState()
+
+        if (data != null) {
+            DataDisplay(data = data!!, searchText)
+            Spacer(modifier = Modifier.height(8.dp))
+            PageIndicator(viewModel)
+        } else {
+            Text("No items available")
+        }
+    }
+}
+
+
+@Composable
+fun DataDisplay(data: ApiResponse, searchText: String) {
+    // 데이터를 보여주는 간단한 카드 Composable
+    if (data.body?.items?.isNotEmpty() == true) {  // 데이터가 있을 때
+        Column {
+            Text("\"$searchText\"에 대한 검색 결과입니다.", modifier = Modifier.fillMaxWidth(), fontSize = 20.sp, textAlign = TextAlign.Center)
+            Spacer(modifier = Modifier.height(30.dp))
+
+            HeaderBar()
+
+            data.body?.items?.forEach { items ->
+                items.item?.forEachIndexed { index, item ->
+                    Row {
+                        Text(text = "${data.body?.pageNo?.toInt()?.minus(1)?.times(10)?.plus(index+1)}", modifier = Modifier.weight(1f))
+                        Text(text = "${item.FOOD_NM_KR}", modifier = Modifier.weight(3f))
+                        Text(text = "${item.AMT_NUM1}", modifier = Modifier.weight(2f))
+                        Button(
+                            onClick = { /*TODO*/ },
+                            shape = CircleShape,
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_add_shopping_cart_24),
+                                contentDescription = "비교하기"
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        Text("No items available")
+    }
+}
+
+@Composable
+fun PageIndicator(viewModel: SearchViewModel) {
+    val totalPages = viewModel.getTotalPages()
+    val data by viewModel.data.collectAsState()
+    val currentPage = data?.body?.pageNo?.toInt() ?: 1
+
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        for (page in 1..totalPages) {
+            Text(
+                text = page.toString(),
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clickable { viewModel.changePage(page) },
+                fontWeight = if (page == currentPage) FontWeight.Bold else FontWeight.Normal,
+                textDecoration = if (page == currentPage) TextDecoration.Underline else TextDecoration.None
+            )
+        }
+    }
+}
+
+
+@Composable
+fun HeaderBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "번호",
+            color = Color.White,
+            modifier = Modifier.weight(1f).padding(start = 8.dp)
+        )
+        Text(
+            text = "식품명",
+            color = Color.White,
+            modifier = Modifier.weight(3f).padding(start = 8.dp)
+        )
+        Text(
+            text = "에너지(kcal)",
+            color = Color.White,
+            modifier = Modifier.weight(2f).padding(start = 8.dp)
+        )
+        Text(
+            text = "비교함",
+            color = Color.White,
+            modifier = Modifier.weight(1f).padding(start = 8.dp)
+        )
+    }
+}
+
---
+++ app/src/main/java/com/example/sjsk/firestore/NutritionInfo.kt
@@ -0,0 +1,118 @@
+package com.example.sjsk.firestore
+
+import android.util.Log
+import com.google.gson.annotations.SerializedName
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.simpleframework.xml.Element
+import org.simpleframework.xml.ElementList
+import org.simpleframework.xml.Root
+import java.io.InputStream
+
+
+//API에서 받을 JSON 데이터에 맞춰 Kotlin 데이터 클래스를 정의
+
+@Root(name = "response", strict = false)
+data class ApiResponse(
+    @field:Element(name = "header", required = false)
+    var header: Header? = null,
+
+    @field:Element(name = "body", required = false)
+    var body: Body? = null
+)
+
+@Root(name = "header", strict = false)
+data class Header(
+    @field:Element(name = "resultCode", required = false)
+    var resultCode: String = "",
+
+    @field:Element(name = "resultMsg", required = false)
+    var resultMsg: String = "",
+)
+
+@Root(name = "body", strict = false)
+data class Body(
+    @field:Element(name = "numOfRows", required = false)
+    var numOfRows: String? = null,
+
+    @field:Element(name = "pageNo", required = false)
+    var pageNo: String = "",
+
+    @field:Element(name = "totalCount", required = false)
+    var totalCount: String = "",
+
+    @field:ElementList(entry = "items", required = false, inline = true)
+    var items: ArrayList<Items>? = null
+)
+
+@Root(name = "items", strict = false)
+data class Items(
+    @field:ElementList(entry = "item", required = false, inline = true)
+    var item: ArrayList<Item>? = null
+)
+
+@Root(name = "item", strict = false)
+data class Item(
+//    val NUM: String, //번호
+
+    @field:Element(name = "FOOD_CD", required = false)
+    var FOOD_CD: String? = null,
+
+    @field:Element(name = "ITEM_REPORT_NO", required = false)
+    var ITEM_REPORT_NO: String? = null,
+
+    @field:Element(name = "FOOD_NM_KR", required = false)
+    var FOOD_NM_KR: String? = null,
+
+    @field:Element(name = "FOOD_OR_NM", required = false)
+    var FOOD_OR_NM: String? = null,
+
+    @field:Element(name = "FOOD_REF_NM", required = false)
+    var FOOD_REF_NM: String? = null,
+
+    @field:Element(name = "FOOD_CAT3_NM", required = false)
+    var FOOD_CAT3_NM: String? = null,
+
+    @field:Element(name = "SERVING_SIZE", required = false)
+    var SERVING_SIZE: String? = null,
+
+    @field:Element(name = "AMT_NUM1", required = false)
+    var AMT_NUM1: String? = null,
+
+    @field:Element(name = "AMT_NUM3", required = false)
+    var AMT_NUM3: String? = null,
+
+    @field:Element(name = "AMT_NUM4", required = false)
+    var AMT_NUM4: String? = null,
+
+    @field:Element(name = "AMT_NUM6", required = false)
+    var AMT_NUM6: String? = null,
+
+    @field:Element(name = "AMT_NUM7", required = false)
+    var AMT_NUM7: String? = null,
+
+    @field:Element(name = "AMT_NUM13", required = false)
+    var AMT_NUM13: String? = null,
+
+    @field:Element(name = "AMT_NUM22", required = false)
+    var AMT_NUM22: String? = null,
+
+    @field:Element(name = "AMT_NUM23", required = false)
+    var AMT_NUM23: String? = null,
+
+    @field:Element(name = "NUTRI_AMOUNT_SERVING", required = false)
+    var NUTRI_AMOUNT_SERVING: String? = null,
+
+    @field:Element(name = "Z10500", required = false)
+    var Z10500: String? = null,
+
+    @field:Element(name = "MAKER_NM", required = false)
+    var MAKER_NM: String? = null,
+
+    @field:Element(name = "UPDATE_YMD", required = false)
+    var UPDATE_YMD: String? = null
+)
+
+
+
+
